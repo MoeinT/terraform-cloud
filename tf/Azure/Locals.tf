b13608d31c3eb6bfadd77d4f6a463afe8e5e2b6a
MoeinT <mohammadmoein.torabi@volvo.com>
2022-08-26 10:39:07 +0200

    added a keyvault and a new policy

--- tf/Azure/Locals.tf
+++ tf/Azure/Locals.tf
@@ -9,4 +9,21 @@ locals {
     "Service_Level"     = "N/A"
     "Technical_Contact" = "thomas.lienard@consultant.volvo.com"
   }
+
+  access_policy = [
+    {
+      tenant_id           = data.azurerm_client_config.current.tenant_id,
+      object_id           = var.sp_object_id,
+      secret_permissions  = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
+      key_permissions     = ["Get", ]
+      storage_permissions = ["Get", ]
+    },
+    {
+      tenant_id           = data.azurerm_client_config.current.tenant_id,
+      object_id           = var.moein_obj_id,
+      secret_permissions  = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
+      key_permissions     = ["Get", ]
+      storage_permissions = ["Get", ]
+    }
+  ]
 }
