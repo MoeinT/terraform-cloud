48aa30c6de94fae686c91b386b335386045067f2
MoeinT <mohammadmoein.torabi@volvo.com>
2022-08-26 18:07:39 +0200

    try

--- tf/Azure/Locals.tf
+++ tf/Azure/Locals.tf
@@ -8,7 +8,6 @@ locals {
     "PDS"               = "Basic"
     "Service_Level"     = "N/A"
     "Technical_Contact" = "thomas.lienard@consultant.volvo.com"
-    "environment"       = "test"
   }
 
   access_policy = [
