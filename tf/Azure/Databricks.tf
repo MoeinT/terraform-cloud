0a8632e3c501af2e31a3f2a50272c3f4bdb8db7b
MoeinT <mohammadmoein.torabi@volvo.com>
2022-08-26 09:43:01 +0200

    added a db cluster

--- tf/Azure/Databricks.tf
+++ tf/Azure/Databricks.tf
@@ -8,9 +8,20 @@ resource "azurerm_databricks_workspace" "databricks-ws" {
 }
 
 provider "databricks" {
-  /* host                        = azurerm_databricks_workspace.databricks-ws.workspace_url
+  host                        = azurerm_databricks_workspace.databricks-ws.workspace_url
   azure_workspace_resource_id = azurerm_databricks_workspace.databricks-ws.id
   azure_client_id             = var.client_id
   azure_client_secret         = var.client_secret
-  azure_tenant_id             = data.azurerm_client_config.current.tenant_id */
+  azure_tenant_id             = data.azurerm_client_config.current.tenant_id
+}
+
+resource "databricks_cluster" "db-cluster" {
+  cluster_name            = "db-${var.suffix}-${var.environment}"
+  spark_version           = "11.1.x-scala2.12"
+  node_type_id            = "Standard_DS3_v2"
+  autotermination_minutes = 20
+  autoscale {
+    min_workers = 1
+    max_workers = 4
+  }
 }
