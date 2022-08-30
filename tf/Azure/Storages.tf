1c2630078b37cf1ac2aba97e39029c171d418e63
Torabi Mohammadmoein <mohammadmoein.torabi@volvo.com>
2022-08-30 15:16:23 +0200

    added a container

--- tf/Azure/Storages.tf
+++ tf/Azure/Storages.tf
@@ -18,3 +18,8 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "fs-streamingData" {
   name               = "commonfiles-${var.environment}"
   storage_account_id = azurerm_storage_account.sq-streamingData.id
 }
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "fs-streamingData1" {
+  name               = "commonfiles1-${var.environment}"
+  storage_account_id = azurerm_storage_account.sq-streamingData.id
+}
